64c5bd4b3f9d99eb89a2786b7549c5c650fd4639
ABMshawonislam-dev <abmshawonislam@gmail.com>
2024-01-26 19:37:34 +0600

    forgot done

--- backend/controllers/changePasswordController.js
+++ backend/controllers/changePasswordController.js
@@ -1,8 +1,26 @@
 let User = require("../model/userSchema")
+var jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 let changePasswordController = async (req,res)=>{
-    let {email,password} = req.body
-    console.log("change password")
+    let {token,password} = req.body
+    console.log(token,password)
+
+    jwt.verify(token, '123234',async function(err, decoded) {
+        console.log(decoded.email)
+        let existingUser2 = await User.findOneAndUpdate({email:decoded.email},{ $set:
+            {
+                "password":password,
+              "token": "",
+            },
+            
+         },{ //options
+          returnNewDocument: true,
+          new: true,
+          strict: false
+        })
+
+      });
      
    
 
--- backend/controllers/fortgotPasswordController.js
+++ backend/controllers/fortgotPasswordController.js
@@ -1,13 +1,14 @@
 let User = require("../model/userSchema")
 const bcrypt = require('bcrypt');
 const nodemailer = require("nodemailer");
+const jwt = require('jsonwebtoken');
 
 
 let forgotPasswordController = async (req,res)=>{
     let {email} = req.body
    
     
- 
+    
 
     let existingUser = await User.find({email:email})
     console.log(existingUser[0].email)
@@ -16,9 +17,20 @@ let forgotPasswordController = async (req,res)=>{
     if(existingUser.length == 0){
         res.send({error:"Credencial is not valid"})
     }else{
-      
+
+
+      jwt.sign({ email: email }, "123234",async function(err, token) {
+          let existingUser2 = await User.updateOne({email:email},{ $set:
+        {
+          token: token,
+        },
         
-        const transporter = nodemailer.createTransport({
+     },{ //options
+      returnNewDocument: true,
+      new: true,
+      strict: false
+    })
+     const transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
               user: "citmern2202@gmail.com",
@@ -30,10 +42,16 @@ let forgotPasswordController = async (req,res)=>{
             from: process.env.BASE_EMAIL, 
             to: existingUser[0].email,
             subject: "Change Password", 
-            html: `<div style="display: flex;width: 600px;height: 200px;"> <div style="width: 50%;height: 100px;">Please change your password by click on this button <a href="http://localhost:5173/changepassword/${existingUser[0].email}">Verify</a></div></div>`,
+            html: `<div style="display: flex;width: 600px;height: 200px;"> <div style="width: 50%;height: 100px;">Please change your password by click on this button <a href="http://localhost:5173/changepassword/${token}">Verify</a></div></div>`,
           });
 
-          res.send({success:"Check your email"})
+        res.send({success:"Check your email"})
+      });
+
+    
+    //   console.log(existingUser)
+        
+       
     }
   
 

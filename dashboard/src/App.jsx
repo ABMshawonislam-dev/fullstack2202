d5de606e947df2c7246bdff63e7ce8531b0353f2
Homelander-69 <abhinashpaul370@gmail.com>
2024-02-03 16:36:24 +0600

    view category
    yes yes yes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,9 +1,8 @@
-
 import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements
+   Route,
+   RouterProvider,
+   createBrowserRouter,
+   createRoutesFromElements,
 } from "react-router-dom";
 import ChangePassword from "./components/ChangePassword";
 import ForgotPassword from "./components/ForgotPassword";
@@ -12,37 +11,34 @@ import OtpPage from "./components/OtpPage";
 import Registration from "./components/Registration";
 import Home from "./components/Home";
 import UserList from "./components/UserList";
+import ViewCategory from "./components/ViewCategory";
 
 function App() {
+   const router = createBrowserRouter(
+      createRoutesFromElements(
+         <Route>
+            <Route path="/" element={<Registration />}></Route>
+            <Route path="/otp/:email" element={<OtpPage />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
+            <Route
+               path="/changepassword/:email"
+               element={<ChangePassword />}
+            ></Route>
 
-  
- 
+            <Route path="/home" element={<Home />}>
+               <Route path="userlist" element={<UserList />}></Route>
+               <Route path="viewcategory" element={<ViewCategory />}></Route>
+            </Route>
+         </Route>
+      )
+   );
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route path="/" element={<Registration />}></Route>
-        <Route path="/otp/:email" element={<OtpPage />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
-        <Route
-          path="/changepassword/:email"
-          element={<ChangePassword />}
-        ></Route>
-
-        <Route path="/home" element={<Home />}>
-          <Route path="userlist" element={<UserList />}></Route>
-        </Route>
-      </Route>
-    )
-  );
-
-  return (
-    <>
-    <RouterProvider router={router} />
-     
-    </>
-  )
+   return (
+      <>
+         <RouterProvider router={router} />
+      </>
+   );
 }
 
-export default App
+export default App;
